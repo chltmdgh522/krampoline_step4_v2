34b77e6b7c0a60737ee3055b18503b617089e4c7
chltmdgh522 <chltmdgh517@naver.com>
2024-11-01 02:48:43 +0900

    ㄴㅁㄹㅇㄴㅁㅇㄹ

--- k8s/configs/init.sql
+++ k8s/configs/init.sql
@@ -6,11 +6,30 @@ FLUSH PRIVILEGES;
 
 USE `krampoline`;
 
-DROP TABLE IF EXISTS `sample_data`;
-CREATE TABLE `sample_data` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `detail` varchar(100) NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO sample_data (`id`,`detail`) VALUES ('1', 'Hello DKOS!');
+CREATE TABLE `member` (
+                          `member_id` VARCHAR(255) NOT NULL,
+                          `phone` VARCHAR(255) NULL,
+                          PRIMARY KEY (`member_id`)
+) DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `note` (
+                        `id` BIGINT NOT NULL AUTO_INCREMENT,
+                        `alarm` BIT NOT NULL,
+                        `execute` BIT NOT NULL,
+                        `favorite` BIT NOT NULL,
+                        `favorite_pre` INT NOT NULL,
+                        `frequency` INT NOT NULL,
+                        `station` INT NOT NULL,
+                        `departure` VARCHAR(255) NULL,
+                        `destination` VARCHAR(255) NULL,
+                        `member_id` VARCHAR(255) NULL,
+                        `notion_id` VARCHAR(255) NULL,
+                        `station_id` VARCHAR(255) NULL,
+                        `time` VARCHAR(255) NULL,
+                        PRIMARY KEY (`id`),
+                        CONSTRAINT `FK_note_member`
+                            FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`)
+) DEFAULT CHARSET=utf8mb4;
+
+
